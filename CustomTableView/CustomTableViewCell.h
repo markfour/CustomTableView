ed87354eda45c8f1c93947bfca816d771348c82c
hayashidakazumi <axhfaro@gmail.com>
2015-07-19 21:46:44 +0900

    add CustomTableViewCell

---
+++ CustomTableView/CustomTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewCell.h
+//  CustomTableView
+//
+//  Created by HayashidaKazumi on 2015/07/19.
+//  Copyright (c) 2015年 HayashidaKazumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableViewCell : UITableViewCell
+
+@end
---
+++ CustomTableView/CustomTableViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewCell.m
+//  CustomTableView
+//
+//  Created by HayashidaKazumi on 2015/07/19.
+//  Copyright (c) 2015年 HayashidaKazumi. All rights reserved.
+//
+
+#import "CustomTableViewCell.h"
+
+@implementation CustomTableViewCell
+
+@end
